e7bbe43e88939259ba851faa9b847edf03fcfbe9
mooz <stillpedant@gmail.com>
2011-09-20 21:51:47 +0900

    Use online org.js

--- stub/editor/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DIR_EDITOR=$(cd $(dirname $0); pwd)
-DIR_EXPORTER=$DIR_EDITOR/../module-exporter
-DIR_ORGJS=$DIR_EDITOR/../../src/org-js
-
-cd $DIR_EXPORTER
-
-./export.rb ../../src/org-js/*.js > ../editor/org.js
