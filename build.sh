3dcc800f9030b830e25ed5f88a6f2a2a68f20d64
mooz <stillpedant@gmail.com>
2012-02-27 15:27:17 +0900

    Support Node.js

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 DIR_BASE=$(cd $(dirname $0); pwd)
 DIR_EXPORTER=$DIR_BASE/stub/module-exporter
-DIR_ORGJS=$DIR_BASE/src/org-js
+DIR_ORGJS=$DIR_BASE/lib/org
 
 $DIR_EXPORTER/export.rb $DIR_ORGJS/*.js > ./org.js
