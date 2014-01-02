mooz/org-js

commit 6bc2bf829e455bc9bd197ed429187d796f3826d8

Redesign converters

lib/org/converter/converter.js
@@ -1,0 +1,273 @@
+var Node = require("../node.js").Node;
+
+function Converter() {
+}
+
+Converter.prototype = {
+  exportOptions: {
+    headerOffset: 1,
+    exportFromLineNumber: false,
+    suppressSubScriptHandling: false,
+    suppressAutoLink: false
+  },
+
+  untitled: "Untitled",
+  convertResult: null,
+
+  // TODO: Manage TODO lists
+
+  initialize: function (orgDocument, exportOptions) {
+    this.orgDocument = orgDocument;
+    this.documentOptions = orgDocument.options || {};
+    this.exportOptions = exportOptions || {};
+
+    this.headers = [];
+    this.headerOffset =
+      typeof this.exportOptions.headerOffset === "number" ? this.exportOptions.headerOffset : 1;
+    this.sectionNumbers = [0];
+  },
+
+  computeToc: function (exportTocLevel) {
+    if (typeof exportTocLevel !== "number")
+      exportTocLevel = Infinity;
+
+    var toc = [];
+    toc.parent = null;
+
+    var previousLevel = null;
+    var currentChildren = toc;  // first
+
+    for (var i = 0; i < this.headers.length; ++i) {
+      var headerNode = this.headers[i];
+
+      if (headerNode.level > exportTocLevel)
+        continue;
+
+      if (previousLevel !== null) {
+        var levelDiff = headerNode.level - previousLevel;
+        if (levelDiff > 0) {
+          for (var j = 0; j < levelDiff; ++j) {
+            currentChildren = currentChildren[currentChildren.length - 1].children;
+          }
+        } else if (levelDiff < 0) {
+          levelDiff = -levelDiff;
+          for (var k = 0; k < levelDiff; ++k) {
+            currentChildren = currentChildren.parent;
+          }
+        }
+      }
+
+      var children = [];
+      children.parent = currentChildren;
+      var tocItem = { headerNode: headerNode, children: children };
+      currentChildren.push(tocItem);
+
+      previousLevel = headerNode.level;
+    }
+
+    return toc;
+  },
+
+  convertNode: function (node, recordHeader) {
+    var childText = node.children ? this.convertNodes(node.children, recordHeader) : "";
+    var text;
+
+    var auxData = this.computeAuxDataForNode(node);
+
+    switch (node.type) {
+    case Node.types.header:
+      // Parse task status
+      var taskStatus = null;
+      if (childText.indexOf("TODO ") === 0)
+        taskStatus = "todo";
+      else if (childText.indexOf("DONE ") === 0)
+        taskStatus = "done";
+
+      // Compute section number
+      var sectionNumberText = null;
+      if (recordHeader) {
+        var thisHeaderLevel = node.level;
+        var levelDiff = null;
+        var previousHeaderLevel = this.sectionNumbers.length;
+        if (thisHeaderLevel > previousHeaderLevel) {
+          // TODO: fill out skipped elements with 0 (by performing for loop)
+          this.sectionNumbers[thisHeaderLevel - 1] = 0; // Extend
+        } else if (thisHeaderLevel < previousHeaderLevel) {
+          this.sectionNumbers.length = thisHeaderLevel; // Collapse
+        }
+        this.sectionNumbers[thisHeaderLevel - 1]++;
+        sectionNumberText = this.sectionNumbers.join(".");
+        node.sectionNumberText = sectionNumberText; // Can be used in ToC
+      }
+
+      text = this.convertHeader(node, childText, auxData,
+                                taskStatus, sectionNumberText);
+
+      if (recordHeader)
+        this.headers.push(node);
+      break;
+    case Node.types.orderedList:
+      text = this.convertOrderedList(node. childText, auxData);
+      break;
+    case Node.types.unorderedList:
+      text = this.convertUnorderedList(node. childText, auxData);
+      break;
+    case Node.types.definitionList:
+      text = this.convertDefinitionList(node. childText, auxData);
+      break;
+    case Node.types.listElement:
+      if (node.isDefinitionList) {
+        var termText = this.convertNodes(node.term, recordHeader);
+        text = this.convertDefinitionItem(node, childText, auxData,
+                                          termText, childText);
+      } else {
+        text = this.convertListItem(node, childText, auxData);
+      }
+      break;
+    case Node.types.paragraph:
+      text = this.convertParagraph(node, childText, auxData);
+      break;
+    case Node.types.preformatted:
+      text = this.convertPreformatted(node, childText, auxData);
+      break;
+    case Node.types.table:
+      text = this.convertTable(node, childText, auxData);
+      break;
+    case Node.types.tableRow:
+      text = this.convertTableRow(node, childText, auxData);
+      break;
+    case Node.types.tableCell:
+      if (node.isHeader)
+        text = this.convertTableHeader(node, childText, auxData);
+      else
+        text = this.convertTableCell(node, childText, auxData);
+      break;
+    case Node.types.horizontalRule:
+      text = this.convertHorizontalRule(node, childText, auxData);
+      break;
+      // ============================================================ //
+      // Inline
+      // ============================================================ //
+    case Node.types.inlineContainer:
+      text = this.convertInlineContainer(node, childText, auxData);
+      break;
+    case Node.types.bold:
+      text = this.convertBold(node, childText, auxData);
+      break;
+    case Node.types.italic:
+      text = this.convertItalic(node, childText, auxData);
+      break;
+    case Node.types.underline:
+      text = this.convertUnderline(node, childText, auxData);
+      break;
+    case Node.types.code:
+      text = this.convertCode(node, childText, auxData);
+      break;
+    case Node.types.dashed:
+      text = this.convertDashed(node, childText, auxData);
+      break;
+    case Node.types.link:
+      text = this.convertLink(node, childText, auxData);
+      break;
+    case Node.types.directive:
+      switch (node.directiveName) {
+      case "quote":
+        text = this.convertQuote(node, childText, auxData);
+        break;
+      case "example":
+        text = this.convertExample(node, childText, auxData);
+        break;
+      case "src":
+        text = this.convertSrc(node, childText, auxData);
+        break;
+      default:
+        text = childText;
+      }
+      break;
+    default:
+      var baseText = null;
+      if (node.type === Node.types.text) {
+        baseText = node.value;
+      } else if (typeof node === "string") {
+        baseText = node;
+      } else {
+        throw "Unknown node type: " + node.type;
+      }
+      text = this.convertText(baseText);
+      break;
+    }
+
+    if (typeof this.postProcess === "function") {
+      text = this.postProcess(node, text);
+    }
+
+    return text;
+  },
+
+  convertText: function (text) {
+    var escapedText = this.escapeSpecialChars(text);
+    if (!this.exportOptions.suppressSubScriptHandling)
+      escapedText = this.makeSubscripts(escapedText);
+    if (!this.exportOptions.suppressAutoLink)
+      escapedText = this.linkURL(escapedText);
+    return escapedText;
+  },
+
+  convertNodes: function (nodes, recordHeader) {
+    return nodes.map(function (node) {
+      return this.convertNode(node, recordHeader);
+    }, this).join("");
+  },
+
+  getNodeTextContent: function (node) {
+    if (node.type === Node.types.text)
+      return this.escapeSpecialChars(node.value);
+    else
+      return node.children ? node.children.map(this.getNodeTextContent, this).join("") : "";
+  },
+
+  // @Override
+  escapeSpecialChars: function (text) {
+    throw "Implement escapeSpecialChars";
+  },
+
+  // http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+  urlPattern: /\b(?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])/i,
+
+  // @Override
+  linkURL: function (text) {
+    var self = this;
+    return text.replace(this.urlPattern, function (matched) {
+      if (matched.indexOf("://") < 0)
+        matched = "http://" + matched;
+      return self.makeLink(matched);
+    });
+  },
+
+  makeLink: function (url) {
+    throw "Implement makeLink";
+  },
+
+  makeSubscripts: function (text) {
+    if (this.documentOptions["^"] === "{}")
+      return text.replace(/\b([^_ \t]*)_{([^}]*)}/g,
+                          this.makeSubscript);
+    else if (this.documentOptions["^"])
+      return text.replace(/\b([^_ \t]*)_([^_]*)\b/g,
+                          this.makeSubscript);
+    else
+      return text;
+  },
+
+  makeSubscript: function (match, body, subscript) {
+    throw "Implement makeSubscript";
+  },
+
+  imageExtensionPattern: new RegExp("(" + [
+    "bmp", "png", "jpeg", "jpg", "gif", "tiff",
+    "tif", "xbm", "xpm", "pbm", "pgm", "ppm"
+  ].join("|") + ")$")
+};
+
+if (typeof exports !== "undefined")
+  exports.Converter = Converter;

lib/org.js
@@ -12,5 +12,5 @@ if (typeof exports !== "undefined") {
   exportModule(require("./org/node.js"));
   exportModule(require("./org/parser.js"));
   exportModule(require("./org/stream.js"));
-  exportModule(require("./org/converter-html.js"));
+  exportModule(require("./org/converter/html.js"));
 }

lib/org/converter-html.js
@@ -1,342 +1,0 @@
-var Node = require("./node.js").Node;
-
-var HtmlTextConverter = {
-  convertDocumentToHTML: function (doc, exportOptions) {
-    var convertedDoc = this.convertDocument(doc, exportOptions);
-
-    return convertedDoc.titleHTML +
-      convertedDoc.tocHTML +
-      convertedDoc.this.tag("hr", null) +
-      convertedDoc.contentHTML;
-  },
-
-  convertDocument: function (doc, exportOptions) {
-    this.documentOptions = doc.options || {};
-    this.exportOptions = exportOptions || {};
-    this.initialize(doc);
-
-    var title = doc.title ? this.convertNode(doc.title) : "Untitled";
-    var titleHTML = this.tag("h1", title);
-    var contentHTML = this.convertNodes(doc.nodes, true /* record headers */);
-    var tocHTML = this.generateToc(this.documentOptions["toc"]);
-
-    return {
-      title: title,
-      titleHTML: titleHTML,
-      contentHTML: contentHTML,
-      tocHTML: tocHTML,
-      toString: function () {
-        return titleHTML + tocHTML + contentHTML;
-      }
-    };
-  },
-
-  initialize: function () {
-    this.headers = [];
-    this.headerOffset = typeof this.exportOptions.headerOffset === "number" ? this.headerOffset : 1;
-    this.sectionNumbers = [0];
-  },
-
-  // Call after convertNodes
-  generateToc: function (exportTocLevel) {
-    if (!exportTocLevel)
-      return "";
-
-    if (typeof exportTocLevel !== "number")
-      exportTocLevel = Infinity;
-
-    var toc = [];
-
-    function repeat(text, n) {
-      return Array(n + 1).join(text);
-    }
-
-    var sectionNumbers = [0];
-    var unclosedUlCount = 0;
-    var previousLevel = 0;
-    for (var i = 0; i < this.headers.length; ++i) {
-      var headerNode = this.headers[i];
-
-      if (headerNode.level > exportTocLevel)
-        continue;
-
-      var levelDiff = headerNode.level - previousLevel;
-      if (levelDiff > 0) {
-        toc.push(repeat("<ul>", levelDiff));
-        unclosedUlCount += levelDiff;
-        sectionNumbers[headerNode.level - 1] = 0;
-      } else if (levelDiff < 0) {
-        levelDiff = -levelDiff;
-        toc.push(repeat("</ul>", levelDiff));
-        unclosedUlCount -= levelDiff;
-        sectionNumbers.length = headerNode.level;
-      }
-
-      sectionNumbers[sectionNumbers.length - 1]++;
-
-      var sectionNumber = sectionNumbers.join(".");
-      var sectionNumberString = this.documentOptions.num ?
-            this.inlineTag("span", sectionNumber, { "class": "section-number" }) : "";
-      var headerString = this.convertNodes(headerNode.children);
-      var headerLink = this.inlineTag("a", sectionNumberString + headerString, { href: "#header-" + i });
-
-      toc.push(this.tag("li", headerLink));
-      previousLevel = headerNode.level;
-    }
-
-    // Close remained <ul>
-    if (unclosedUlCount > 0)
-      toc.push(repeat("</ul>", unclosedUlCount));
-
-    return this.tag("div", toc.join(""), { id: "org-toc" });
-  },
-
-  getNodeTextContent: function (node) {
-    if (node.type === Node.types.text)
-      return this.escapeTags(node.value);
-    else
-      return node.children ? node.children.map(this.getNodeTextContent, this).join("") : "";
-  },
-
-  computeAuxAttribute: function (node) {
-    while (node.parent &&
-           node.parent.type === Node.types.inlineContainer) {
-      node = node.parent;
-    }
-    var attributesNode = node.previousSibling;
-    var attributesText = "";
-    while (attributesNode &&
-           attributesNode.type === Node.types.directive &&
-           attributesNode.directiveName === "attr_html:") {
-      attributesText += attributesNode.directiveRawValue + " ";
-      attributesNode = attributesNode.previousSibling;
-    }
-    return attributesText;
-  },
-
-  convertNode: function (node, recordHeader) {
-    var childText = node.children ? this.convertNodes(node.children, recordHeader) : "";
-    var text;
-
-    var auxAttributesText = this.computeAuxAttribute(node);
-
-    switch (node.type) {
-    case Node.types.header:
-      // Add section number
-      if (this.documentOptions.num && recordHeader) {
-        var thisHeaderLevel = node.level;
-        var levelDiff = null;
-        var previousHeaderLevel = this.sectionNumbers.length;
-
-        if (thisHeaderLevel > previousHeaderLevel)
-          this.sectionNumbers[thisHeaderLevel - 1] = 0; // Extend (TODO: fill out skipped elements with 0)
-        else if (thisHeaderLevel < previousHeaderLevel)
-          this.sectionNumbers.length = thisHeaderLevel; // Collapse
-
-        this.sectionNumbers[thisHeaderLevel - 1]++;
-
-        childText = this.inlineTag("span", this.sectionNumbers.join("."), {
-          "class": "section-number"
-        }) + childText;
-      }
-
-      text = this.tag("h" + (this.headerOffset + thisHeaderLevel),
-                      childText,
-                      { id: "header-" + this.headers.length },
-                      auxAttributesText);
-      if (recordHeader)
-        this.headers.push(node);
-      break;
-    case Node.types.orderedList:
-      text = this.tag("ol", childText, null, auxAttributesText);
-      break;
-    case Node.types.unorderedList:
-      text = this.tag("ul", childText, null, auxAttributesText);
-      break;
-    case Node.types.definitionList:
-      text = this.tag("dl", childText, null, auxAttributesText);
-      break;
-    case Node.types.listElement:
-      if (node.isDefinitionList) {
-        var termText = this.convertNodes(node.term, recordHeader);
-        text = this.tag("dt", termText) + this.tag("dd", childText);
-      } else {
-        text = this.tag("li", childText, null, auxAttributesText);
-      }
-      break;
-    case Node.types.paragraph:
-      text = this.tag("p", childText, null, auxAttributesText);
-      break;
-    case Node.types.preformatted:
-      text = this.tag("pre", childText, null, auxAttributesText);
-      break;
-    case Node.types.table:
-      // TODO: Consider <col> or <colgroup>
-      text = this.tag("table", this.tag("tbody", childText), null, auxAttributesText);
-      break;
-    case Node.types.tableRow:
-      text = this.tag("tr", childText);
-      break;
-    case Node.types.tableCell:
-      if (node.isHeader)
-        text = this.tag("th", childText);
-      else
-        text = this.tag("td", childText);
-      break;
-    case Node.types.horizontalRule:
-      text = this.tag("hr", null, null, auxAttributesText);
-      break;
-      // ============================================================ //
-      // Inline
-      // ============================================================ //
-    case Node.types.inlineContainer:
-      text = childText;
-      break;
-    case Node.types.bold:
-      text = this.inlineTag("b", childText);
-      break;
-    case Node.types.italic:
-      text = this.inlineTag("i", childText);
-      break;
-    case Node.types.underline:
-      text = this.inlineTag("span", childText, { style: "text-decoration:underline;" });
-      break;
-    case Node.types.code:
-      text = this.inlineTag("code", childText);
-      break;
-    case Node.types.dashed:
-      text = this.inlineTag("del", childText);
-      break;
-    case Node.types.link:
-      if (this.imageExtensionPattern.exec(node.src)) {
-        var imgText = this.getNodeTextContent(node);
-        text = this.inlineTag("img",
-                              null,
-                              { src: node.src, alt: imgText, title: imgText },
-                              auxAttributesText);
-      } else
-        text = this.inlineTag("a", childText, { href: node.src });
-      break;
-    case Node.types.directive:
-      var tagName;
-      var tagOptions = {};
-
-      switch (node.directiveName) {
-      case "quote":
-        tagName = "blockquote";
-        break;
-      case "example":
-        tagName = "pre";
-        break;
-      case "src":
-        tagName = "pre";
-        tagOptions["class"] = "prettyprint";
-
-        var codeLanguage = node.directiveArguments.length
-              ? node.directiveArguments[0]
-              : "unknown";
-        childText = this.tag("code",
-                             childText,
-                             { "class": "language-" + codeLanguage },
-                             auxAttributesText);
-        break;
-      }
-
-      if (tagName)
-        text = this.tag(tagName, childText, tagOptions);
-      else
-        text = childText;
-      break;
-    case Node.types.text:
-      var tagEscapedText = this.escapeTags(node.value);
-      if (!this.exportOptions.suppressSubScriptHandling)
-        tagEscapedText = this.makeSubscripts(tagEscapedText);
-      text = this.linkURL(tagEscapedText);
-      break;
-    default:
-      if (typeof node === "string")
-        text = this.linkURL(this.makeSubscripts(this.escapeTags(node)));
-      break;
-    }
-
-    if (this.exportOptions.exportFromLineNumber && typeof node.fromLineNumber === "number") {
-      text = this.inlineTag("div", text, {
-        "data-line-number": node.fromLineNumber
-      });
-    }
-
-    return text;
-  },
-
-  attributesObjectToString: function (attributesObject) {
-    var attributesString = "";
-    for (var attributeName in attributesObject) {
-      if (attributesObject.hasOwnProperty(attributeName)) {
-        attributesString += " " + attributeName + "=\"" + attributesObject[attributeName] + "\"";
-      }
-    }
-    return attributesString;
-  },
-
-  inlineTag: function (name, innerText, attributesObject, auxAttributesText) {
-    attributesObject = attributesObject || {};
-
-    var htmlString = "<" + name;
-    // TODO: check duplicated attributes
-    if (auxAttributesText)
-      htmlString += " " + auxAttributesText;
-    htmlString += this.attributesObjectToString(attributesObject);
-
-    if (innerText === null)
-      return htmlString + "/>";
-
-    htmlString += ">" + innerText + "</" + name + ">";
-
-    return htmlString;
-  },
-
-  tag: function (name, innerText, attributesObject, auxAttributesText) {
-    return this.inlineTag(name, innerText, attributesObject, auxAttributesText) + "\n";
-  },
-
-  // http://daringfireball.net/2010/07/improved_regex_for_matching_urls
-  urlPattern: /\b(?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])/i,
-
-  linkURL: function (text) {
-    return text.replace(this.urlPattern, function (matched) {
-      if (matched.indexOf("://") < 0)
-        matched = "http://" + matched;
-      return "<a href=\"" + matched + "\">" + decodeURIComponent(matched) + "</a>";
-    });
-  },
-
-  makeSubscripts: function (text) {
-    var replacee = "<span class=\"org-subscript-parent\">$1</span><span class=\"org-subscript-child\">$2</span>";
-    if (this.documentOptions["^"] === "{}")
-      return text.replace(/\b([^_ \t]*)_{([^}]*)}/g, replacee);
-    else if (this.documentOptions["^"])
-      return text.replace(/\b([^_ \t]*)_([^_]*)\b/g, replacee);
-    else
-      return text;
-  },
-
-  convertNodes: function (nodes, recordHeader) {
-    return nodes.map(function (node) {
-      return this.convertNode(node, recordHeader);
-    }, this).join("");
-  },
-
-  escapeTags: function (text) {
-    return text.replace(/[&<>"']/g, function (matched) {
-      return "&#" + matched.charCodeAt(0) + ';';
-    });
-  },
-
-  imageExtensionPattern: new RegExp("(" + [
-    "bmp", "png", "jpeg", "jpg", "gif", "tiff",
-    "tif", "xbm", "xpm", "pbm", "pgm", "ppm"
-  ].join("|") + ")$")
-};
-
-if (typeof exports !== "undefined")
-  exports.HtmlTextConverter = HtmlTextConverter;

commit 75bc43eb4a575967664a6994f2813088b77081ce

Parse (un)ordered and definition lists correctly
Fixing a regression in 6bc2bf8

lib/org/converter/converter.js
@@ -107,13 +107,13 @@ Converter.prototype = {
         this.headers.push(node);
       break;
     case Node.types.orderedList:
-      text = this.convertOrderedList(node. childText, auxData);
+      text = this.convertOrderedList(node, childText, auxData);
       break;
     case Node.types.unorderedList:
-      text = this.convertUnorderedList(node. childText, auxData);
+      text = this.convertUnorderedList(node, childText, auxData);
       break;
     case Node.types.definitionList:
-      text = this.convertDefinitionList(node. childText, auxData);
+      text = this.convertDefinitionList(node, childText, auxData);
       break;
     case Node.types.listElement:
       if (node.isDefinitionList) {