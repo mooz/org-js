110d8ebb29b5b826f2ddacd86b1e1599a6da0e21
mooz <stillpedant@gmail.com>
2014-01-02 13:40:33 +0900

    Export converter correctly

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ DIR_BASE=$(cd $(dirname $0); pwd)
 DIR_EXPORTER=$DIR_BASE/stub/module-exporter
 DIR_ORGJS=$DIR_BASE/lib/org
 
-$DIR_EXPORTER/export.rb $DIR_ORGJS/*.js > ./org.js
+$DIR_EXPORTER/export.rb $DIR_ORGJS/*.js $DIR_ORGJS/converter/*.js > ./org.js
